7833f072f497bf15b3843174d9f8d78a2a94e696
Lonna Cash <lcash@ge-j5.kzn.21-school.ru>
2021-06-20 18:20:31 +0300

    123

--- main.c
+++ main.c
@@ -7,8 +7,20 @@
 #include <string.h>
 #include <errno.h>
 
+void	get_addres(char **envp, char ** argv)
+{
+	char *addres;
+	char *tmp;
+	int	i = 0
+	while (envp[i])
+	{
+		envp[i]
+	}
+	
+}
 void	call_cmd1_process(int *fd, char **argv, char **envp)
 {
+		get_addres(envp, argv);
 		int fd_input_file;
 		char result[100] = "/usr/bin/"; // разобраться как проверять доступ
 		if (access(argv[1], O_RDONLY) == -1)
@@ -28,7 +40,7 @@ void	call_cmd1_process(int *fd, char **argv, char **envp)
 		strcat(result, cmd1[0]); // заменить на либовскую функцию
 		if (execve(result, cmd1, envp)  < 0) // проверить на ошибку
 		{
-			printf("CMD1 fail\n");
+			perror("CMD1 fail");
 		}
 }
 
@@ -48,27 +60,25 @@ void	call_cmd2_process(int *fd, char **argv, char **envp)
 
 int	main(int argc, char **argv, char **envp)
 {
-	// int	fd[2];
-	// int	pid1;
-	// int	pid2;
+	int	fd[2];
+	int	pid1;
+	int	pid2;
 
-	// if (pipe(fd) < 0)
-	// 	return (1);
-	// pid1 = fork();
-	// if (pid1 < 0)
-	// 	return (2);
+	if (pipe(fd) < 0)
+		return (1);
+	pid1 = fork();
+	if (pid1 < 0)
+		return (2);
 
-	// if (pid1 == 0) //cmd1 process
-	// 	call_cmd1_process(fd, argv, envp);
-	// pid2 = fork();
-	// if (pid2 < 0)
-	// 	return (3);
-	// if(pid2 == 0) // cmd2 process
-	// 	call_cmd2_process(fd, argv, envp);
-	// close(fd[1]);
-	// close(fd[0]);
-	// wait(NULL);
-
-	printf("%s", envp[0]);
+	if (pid1 == 0) //cmd1 process
+		call_cmd1_process(fd, argv, envp);
+	pid2 = fork();
+	if (pid2 < 0)
+		return (3);
+	if(pid2 == 0) // cmd2 process
+		call_cmd2_process(fd, argv, envp);
+	close(fd[1]);
+	close(fd[0]);
+	wait(NULL);
 }
 
